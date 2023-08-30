50c9838134d7809a7cbc3f7e8d3842afd4bfbe9b
Dmitry Skorodumov <dmitry.skor99@gmail.com>
2023-08-30 16:49:14 +0300

    Многопоточка

--- app/src/main/java/com/example/networkeducation/MainActivity.kt
+++ app/src/main/java/com/example/networkeducation/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.networkeducation
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
@@ -16,20 +19,26 @@ import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
 
-    val MY_TAG = "MyDebugTagSuperClass"
+    private val MY_TAG = "MyDebugTagSuperClass"
+
+    private var button: Button? = null
+    private var progressBar: ProgressBar? = null
+    private val urlString = "https://rickandmortyapi.com/api"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button)
+        progressBar = findViewById(R.id.progressBar)
+        button?.setOnClickListener { requestRickAndMorty() }
 
+    }
 
-        val urlString = "https://rickandmortyapi.com/api"
-
-
-
-
-
-        Thread(object : Runnable {
+    private fun requestRickAndMorty() {
+        button?.isVisible = false
+        progressBar?.isVisible = true
+        val requestThread = Thread(object : Runnable {
             override fun run() {
                 try {
                     // создали объект класса URL и передали в него ссылку
@@ -77,7 +86,64 @@ class MainActivity : AppCompatActivity() {
                     Log.d(MY_TAG, "${t.message}")
                     return
                 }
+
+                runOnUiThread {
+                    button?.isVisible = true
+                    progressBar?.isVisible = false
+                }
             }
-        }).start()
+        })
+
+        requestThread.name = "RickAndMortyThread"
+        requestThread.start()
     }
 }
+
+
+
+// Примитивный пример потока. из особенностей. Класс MyLooper нельзя создать из вне.
+// Именно поток его создает
+class MyThread constructor(private val runnable: Runnable) {
+    private val looper: MyLooper = MyLooper()
+
+    fun start() {
+        post(runnable)
+        looper.start()
+    }
+
+    fun post(runnable: Runnable) {
+        looper.post(runnable)
+    }
+
+    // класс с бесконечным циклом, опрашивающий очередь задач
+    private class MyLooper {
+
+
+        private val messageQueue = mutableListOf<Runnable>()
+        fun start() {
+            while (true) {
+                if (messageQueue.isNotEmpty()) {
+                    messageQueue.first().run()
+                    messageQueue.removeAt(0)
+                }
+            }
+        }
+
+        fun post(runnable: Runnable) {
+            messageQueue.add(runnable)
+        }
+
+        fun postDelay(runnable: Runnable, delay: Long) {
+            messageQueue.add(runnable)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
